eaf4bccfd21b903d28392572b1a854f097b6d2ba
ftlll <857785996@qq.com>
2025-09-29 03:18:10 -0400

    add gin router and service

--- main.go
+++ main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
+	"flashchat/router"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		// Log a fatal error if the .env file cannot be loaded
-		// NOTE: If you deploy your app in a production environment
-		// where variables are set by the OS/container, you might
-		// want to handle this error differently (e.g., just log a warning).
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-	Host := os.Getenv("DB_HOST")
-	fmt.Println(Host)
+	r := router.Router()
+	r.Run()
 }
